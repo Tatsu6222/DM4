c8d2296eb46cc181c9deec7ce0d4555672103fcc
Tatsu6222 <87134820+Tatsu6222@users.noreply.github.com>
2021-07-08 23:31:33 +0900

    Initial revision

---
+++ ex4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  int x=0,y=0,z;
+
+  printf("Rolling the dice...\n");
+
+  while(1>=x|x>=6)x = rand();
+
+  printf("Die 1: %d\n",x);
+
+  while(1>=y|y>=6)y = rand();
+
+  printf("Die 2: %d\n",y);
+
+  z = x + y;
+  printf("Total value: %d\n",z);
+
+  return 0;
+}
